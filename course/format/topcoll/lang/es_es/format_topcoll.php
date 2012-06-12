43d17be409a85578b4a8e54dd1d3555b7cdc33ef
Web Master <webmaster@whitireia.ac.nz>
2012-06-12 13:41:08 +1200

    course/format/topcoll: initial commit

---
+++ course/format/topcoll/lang/es_es/format_topcoll.php
@@ -0,0 +1,35 @@
+﻿<?php // $Id: format_topcoll.php,v 1.4 2011/10/06 00:55:38 gb2048 Exp $
+/**
+ * Collapsed Topics Information
+ *
+ * @package    course/format
+ * @subpackage topcoll
+ * @copyright  2009-2011 @ G J Barnard.
+ * @link       http://docs.moodle.org/en/Collapsed_Topics_course_format
+ * @license    http://creativecommons.org/licenses/by-sa/3.0/ Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ */
+
+// Spanish Translation of Collapsed Topics Course Format
+// Traducción al español de formato plegado Curso Temas
+
+// Used by the Moodle Core for identifing the format and displaying in the list of formats for a course in its settings.
+// Utilizado por el Núcleo de Moodle identificando el formato y la visualización en la lista de formatos para un curso en su configuración.
+$string['nametopcoll']='Se derrumbo Temas';
+$string['formattopcoll']='Se derrumbo Temas';
+
+// Used in format.php
+// Utilizado en format.php
+$string['topcolltoggle']='Activar';
+$string['topcolltogglewidth']='width: 26px;';
+
+// Toggle all - Moodle Tracker CONTRIB-3190
+$string['topcollall']='cambia.';
+$string['topcollopened']='Abierto todo';
+$string['topcollclosed']='Cierre todas las';
+
+// Moodle 2.0 Enhancement - Moodle Tracker MDL-15252, MDL-21693 & MDL-22056 - http://docs.moodle.org/en/Development:Languages
+// Moodle 2.0 Mejora - Moodle Tracker MDL-15252, MDL-21693 & MDL-22056 - http://docs.moodle.org/en/Development:Languages
+$string['sectionname'] = 'Tema';
+$string['pluginname'] = 'Se derrumbo Temas';
+$string['section0name'] = 'General';
+?>
---
+++ course/format/topcoll/lang/pt/format_topcoll.php
@@ -0,0 +1,35 @@
+﻿<?php // $Id: format_topcoll.php,v 1.5 2011/10/06 00:55:38 gb2048 Exp $
+/**
+ * Collapsed Topics Information
+ *
+ * @package    course/format
+ * @subpackage topcoll
+ * @copyright  2009-2011 @ G J Barnard.
+ * @link       http://docs.moodle.org/en/Collapsed_Topics_course_format
+ * @license    http://creativecommons.org/licenses/by-sa/3.0/ Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ */
+
+// Portuguese Translation of Collapsed Topics Course Format
+// Português Tradução de Tópicos Fechado formato do curso
+
+// Used by the Moodle Core for identifing the format and displaying in the list of formats for a course in its settings.
+// Usado pelo Core Moodle para identificando o formato e exibição na lista de formatos para um curso em suas configurações.
+$string['nametopcoll']='Fechado Topicos';
+$string['formattopcoll']='Fechado Topicos';
+
+// Used in format.php
+// Usado em format.php
+$string['topcolltoggle']='Alternar';
+$string['topcolltogglewidth']='width: 34px;';
+
+// Toggle all - Moodle Tracker CONTRIB-3190
+$string['topcollall']='todas as alterna.';
+$string['topcollopened']='Abra';
+$string['topcollclosed']='Feche';
+
+// Moodle 2.0 Enhancement - Moodle Tracker MDL-15252, MDL-21693 & MDL-22056 - http://docs.moodle.org/en/Development:Languages
+// Moodle 2.0 Realce - Moodle Tracker MDL-15252, MDL-21693 & MDL-22056 - http://docs.moodle.org/en/Development:Languages
+$string['sectionname'] = 'Assunto';
+$string['pluginname'] = 'Fechado Topicos';
+$string['section0name'] = 'Geral';
+?>
